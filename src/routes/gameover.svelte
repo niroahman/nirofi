d5bed07288eb825aad36e83ae86ec7b889ab8d59
Niro Åhman <niro.ahman@cinia.fi>
2022-03-30 21:26:03 +0300

    Added gameover screen as routing example

---
+++ src/routes/gameover.svelte
@@ -0,0 +1,27 @@
+<script lang="ts"></script>
+
+<main><h1>GAME OVER</h1></main>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+
+	main {
+		background-color: black;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 100vh;
+		background-color: black;
+		color: white;
+		font-family: 'Press Start 2P', cursive;
+		font-size: 14px;
+	}
+
+	h1 {
+		margin-top: 100px;
+		color: rgb(255, 0, 119);
+		font-family: 'Press Start 2P', cursive;
+		font-size: 72px;
+	}
+</style>
