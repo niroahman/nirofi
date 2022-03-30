c976868d972a9baa578a7ff5deffdbb85648530e
Niro Åhman <niro.ahman@cinia.fi>
2022-03-30 21:28:57 +0300

    Refreshed ignored files

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/error.svelte.js';
-
-export { module };
-export const entry = 'error.svelte-ce9f9ecc.js';
-export const js = ["error.svelte-ce9f9ecc.js","chunks/vendor-56c40f56.js"];
-export const css = ["assets/vendor-292344e0.css"];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const entry = 'pages/index.svelte-fc4ba3cf.js';
-export const js = ["pages/index.svelte-fc4ba3cf.js","chunks/vendor-56c40f56.js"];
-export const css = ["assets/pages/index.svelte-5d0b135b.css","assets/vendor-292344e0.css"];
